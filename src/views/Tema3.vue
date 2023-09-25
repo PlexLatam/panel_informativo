c210a4e81c069ea3d9426659b4992072ad85d42a
jorozcop <maechavarriaor@gmail.com>
2023-09-25 16:22:34 -0500

    ajustes
    ajustes

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -8,14 +8,15 @@
       h1 ¿Como solicitar series y peliculas en el canal de Telegram?
 
     .row.justify-content-center.align-items-center.mb-5
-      .col-5.col-lg-2
+      .col-5.col-lg-3
         figure.mb-4
-          img(src='@/assets/curso/temas/tema3/img1.png', alt='Imágen decorativa')
+          img(src='@/assets/curso/temas/tema3/img2.png', alt='Imágen decorativa')
 
-      .col-lg-10
+      .col-lg-9
         p En nuestro grupo de Telegram, te ofrecemos la oportunidad de solicitar películas y series de manera extremadamente sencilla. Además de eso, encontrarás información actualizada sobre todas las series, películas y otros contenidos que se incorporan diariamente a nuestra plataforma. Así que, únete a nosotros para disfrutar de un flujo constante de entretenimiento y mantenerse al tanto de las últimas novedades en el mundo del cine y la televisión. ¡Estamos aquí para brindarte una experiencia de entretenimiento completa!
 
-        h4 Ingresa con el siguiente link a nuestro <a href="https://t.me/+akakd966aoUzODQ5" target="_blank"> <b style= "color:#37aee2;">GRUPO EN TELEGRAM</b> </a>
+        h4 Ingresa con el siguiente link a nuestro <a href="https://t.me/+akakd966aoUzODQ5" target="_blank"> <b style= "color:#029ae5;">GRUPO EN TELEGRAM</b> </a>
+
 
 
 

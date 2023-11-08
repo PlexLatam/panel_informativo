97d5c79d1c4bfd88b4fe714ddb245fe1129a3ff9
jorozcop <maechavarriaor@gmail.com>
2023-11-07 22:17:48 -0500

    ajustes
    ajustes

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -29,7 +29,7 @@
             p.texto-blanco Podras disfrutar PLEX en :  Smart TV, Roku, PC Windows, Pc Mac, TV-Box, Apple tv, Chromecast, Xbox , Playstation, Amazon Fire tv.
 
           .row(titulo="<b style= 'color: #ffffff;'>¿EN CUANTOS DISPOSITIVOS A LA VEZ?</b>")
-            p.texto-blanco Puedes ver hasta en 4 dispositivos simultáneamente dependiendo del plan que hallas adquirido.
+            p.texto-blanco Puedes ver en 2 o en 4 dispositivos simultáneamente dependiendo del plan que hallas adquirido.
 
           .row(titulo="<b style= 'color: #ffffff;'>¿CUALES SON LOS MEDIOS DE PAGO?</b>")
             p.texto-blanco Contamos con diferentes medios de pago tales como
@@ -46,14 +46,21 @@
                 p.mb-0.texto-blanco  <b>Nequi o Daviplata:</b> 3222527461
 
 
-          .row(titulo="<b style= 'color: #ffffff;'>¿SI ME REGISTRO POR MI CUENTA EN PLEX.COM TENDRE ACCESO Al CONTENIDO PREMIUM?</b>")
-            p.texto-blanco En PLEX, el contenido premium se encuentra en servidores privados que no están disponibles para el registro público. Para acceder a este contenido, ofrecemos nuestro servicio de PLEX LATAM, el cual te da acceso a todo el contenido de nuestra plataforma. Regístrate con nosotros y podrás disfrutar de todo nuestro contenido.
+          .row(titulo="<b style= 'color: #ffffff;'>¿SI ME REGISTRO POR MI CUENTA EN PLEX.TV TENDRE ACCESO AL CONTENIDO PREMIUM?</b>")
+            //p.texto-blanco En PLEX, el contenido premium se encuentra en servidores privados que no están disponibles para el registro público. Para acceder a este contenido, ofrecemos nuestro servicio de PLEX LATAM, el cual te da acceso a todo el contenido de nuestra plataforma. Regístrate con nosotros y podrás disfrutar de todo nuestro contenido.
+
+            P.texto-blanco El contenido premium (Estrenos, Series y Películas) de PLEX se encuentra en servidores privados. Para acceder a este, te ofrecemos ofrecemos nuestro servicio de PLEX LATAM. Regístrate con nosotros y podrás disfrutar de todo nuestro contenido.
+          
 
           .row(titulo="<b style= 'color: #ffffff;'>¿EL PAGO ES MENSUAL?</b>")
             p.texto-blanco El Pago de este servicio es mensual, y tienes la opcion de cancelarlo cuando quieras ya que no hay contratos de permanencia.
 
           .row(titulo="<b style= 'color: #ffffff;'>¿CUANTA VELOCIDAD DE INTERNET NECESITO?</b>")
             p.texto-blanco Para reproducir el contenido de este servidor perfectamente se recomienda una velocidad de 10 Mb en Adelante.
+
+
+    h2.text-center para mas informacion contactate con nosotros: 
+    p.text-center <b>Escríbenos a nuestro</b> <a href="https://wa.me/message/HYSMMEVEIBCYI1" target="_blank"> <b style= "color:#3bbf4d;">WhatsApp</b> </a> <b>o visita nuestro</b> <a href="https://instagram.com/plex_latam_oficial?utm_source=qr&igshid=MThlNWY1MzQwNA==" target="_blank"> <b style= "color:#fe008f;">Instagram</b> </a>
 
 
 
